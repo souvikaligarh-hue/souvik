c9231dab325dee0bf6b72074c178767e8ac50759
souvikaligarh-hue <souvikaligarh@gmail.com>
2025-10-02 16:08:53 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,4 @@
+# Personalize greeting based on user input
+name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+print("Hello, "+name+" "+last_name+ "! have a nice day! Welcome to Python programming.")
