4c40d067a3f7700a32aa0e930f4b5c8e39270f0a
souvikaligarh-hue <souvikaligarh@gmail.com>
2025-10-02 16:00:12 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+# Task two numbers as input from the users
+# Perform the basic mathematical operations on two numbers
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+# Addition of two numbers
+print("Addition: ",a+b)
+# Subtraction of two numbers
+print("Subtraction: ",a-b)
+# Multiplication of two numbers
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+
+
+
