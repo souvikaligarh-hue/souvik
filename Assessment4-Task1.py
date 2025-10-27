b697ab0a3071b9498bfa484468a604a374a419a7
souvikaligarh-hue <souvikaligarh@gmail.com>
2025-10-27 12:34:29 +0530

    Add files via upload

---
+++ Assessment4-Task1.py
@@ -0,0 +1,16 @@
+# Task-1 Read a File and Handle Errors
+# Problem Statement: Write a Python program that:
+# 1. Opens and reads a text file named sample.txt.
+# 2. Prints its content line by line.
+# 3. Handles errors gracefully if the file does not exist.
+
+# Opens and reads a text file named sample.txt.
+# If the file exists:
+try:
+    with open('sample.txt', 'r') as file:
+        # Prints its content line by line.
+        for line in file:
+            print(line.strip())
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
+    
